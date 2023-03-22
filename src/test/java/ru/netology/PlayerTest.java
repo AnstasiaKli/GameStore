a1f9190c9849b4d7f90bde67d9f2dadc3b36b369
fps-git <107875518+fps-git@users.noreply.github.com>
2023-03-22 19:09:52 +0100

    Tests for class Player added for checking issues

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -9,13 +11,54 @@ public class PlayerTest {
     public void shouldSumGenreIfOneGame() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-
         Player player = new Player("Petya");
+
         player.installGame(game);
         player.play(game, 3);
 
         int expected = 3;
         int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldUpdatePlayedTime() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Player player = new Player("Petya");
+
+        player.installGame(game);
+
+        int expected = 5;
+        int actual = player.play(game, 5);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowExceptionWithNotInstalledGame() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Крестики - нолики", "Головоломки");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+
+        assertThrows(RuntimeException.class, () -> player.play(game2, 5));
+    }
+
+    @Test
+    public void shouldNotAffectIfAlreadyInstalled() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Крестики - нолики", "Головоломки");
+        Player player = new Player("Petya");
+        player.installGame(game);
+
+        player.play(game, 3);
+        player.installGame(game);
+
+        int expected = 8;
+        int actual = player.play(game, 5);
+
         assertEquals(expected, actual);
     }
 
